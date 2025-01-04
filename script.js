fa75c577cddef720e52825f87733724e9cba1e53
AkashS1507 <akashsahu15072004@gmail.com>
2025-01-04 13:41:44 +0530

    hello #2

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let btn = document.querySelector("button");
 let h1 = document.querySelector("h1");
-
+let click = 1;
 btn.addEventListener("click", () => {
-    h1.innerHTML = "Happy New Year🎉";
-    h1.style.color = "pink";
-    btn.innerHTML = "📭";
+    if(click === 1){
+        h1.innerHTML = "Happy New Year🎉";
+        h1.style.color = "pink";
+        btn.innerHTML = "📭";
+        click = 2;
+    } 
+    else {
+        location.reload();
+    }
 });
